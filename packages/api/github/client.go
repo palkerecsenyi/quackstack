0c9a6b46ba96e1dcb8db7affb33020064a98ae1b
Pal Kerecsenyi <pal@p.kerecs.com>
2024-04-13 19:45:20 +0100

    Add shared github client

---
+++ packages/api/github/client.go
@@ -0,0 +1,17 @@
+package github
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func GetClient(ctx context.Context, accessToken string) *github.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc)
+}

d53c41ee6426bc992a8f850f3fb7cf345748006c
CarolineMorton12 <cm2781@bath.ac.uk>
2024-04-13 15:59:27 +0100

    Initial project container

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="bg-gray-100 p-4">
       <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
       <Navbar />
-      <Project name={"Coding 1"}/>
+      <Project name={"First Project"}/>
     </div>
   );
 }
--- packages/frontend/src/components/Projects.tsx
+++ packages/frontend/src/components/Projects.tsx
@@ -1,6 +1,6 @@
 const Project = ({ name }: { name: string }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <div className="max-w-sm rounded overflow-hidden shadow-sm bg-white m-5">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
       </div>
